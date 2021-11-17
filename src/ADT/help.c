6b784ade0071c387574ea779dd276b6e6a2f1d8f
Vixell Tanjaya <vixell@MacBook-Air-Vixell.local>
2021-11-17 22:03:14 +0700

    tambahin selamat datang sama help

---
+++ src/ADT/help.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+void HELP(){
+    printf("+----------------------------------------------------------------------------------------+ \n");
+    printf("|                                       -Help-                                           | \n");
+    printf("+----------------------------------------------------------------------------------------+ \n");
+    printf("| SKILL	  : Memakai dan membuang skill yang dimiliki                                     | \n");
+    printf("| MAP	  : Menampilkan state peta saat ini                                              | \n");
+    printf("| BUFF	  : Menampilkan daftar buff yang dimiliki pemain                                 | \n");
+    printf("| INSPECT : Menunjukkan informasi teleport pada petak tertentu                           | \n");
+    printf("| ROLL	  : Memutar dadu                                                                 | \n");
+    printf("| SAVE	  : Menyimpan state permainan (Command ini hanya dapat digunakan setelah roll)   | \n");
+    printf("| ENDTURN : Mengakhiri giliran pemain (Command ini hanya dapat digunakan setelah roll)   | \n");
+    printf("| UNDO	  : Mengulang ke ronde sebelumnya                                                | \n");
+    printf("+----------------------------------------------------------------------------------------+ \n");
+
+}
