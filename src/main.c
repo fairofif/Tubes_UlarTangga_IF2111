30c5489aa4a6d83a6461fc72356cbde0aed90d1d
fairofif <fairofif@gmail.com>
2021-11-19 20:56:33 +0700

    add gambaran main

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ int main () {
     /* Declare main variable */
     boolean stopProgram;
     boolean stopGame;
+    boolean nextPlayer;
     int banyakPemain;
     int idxCurrentPlayer;
 
@@ -54,6 +55,7 @@ int main () {
 
         stopGame = FALSE;
         while ((!stopGame) && (!stopProgram)) {
+            nextPlayer = FALSE;
             /* Prosedur UI In Game */
 
             /* input command */
@@ -61,13 +63,95 @@ int main () {
             /* Kalo commandnya HELP */
                 /* Tunjukin command2 yang ada di In Game */
 
+            /* Kalo commandnya SKILL */
+                /* Keluarin SKILL LIST idxCurrentPlayer */
+
+                /* input (int) SKILL yang mau dipake (kalo punya SKILL) */
+
+                /* salin nama skill yang dipake ke variable */
+
+                /* Kalo Masukin SKILL */
+                    /* Kalo SKILL == "Pintu Ga Ke Mana Mana" */
+                        /* Perubahan kondisi di player */
+
+                        /* Perbuahan kondisi di Map */
+                    
+                    /* Kalo SKILL == "Mesin Waktu" */
+                        /* Masukin username yang mau dimundurin */
+
+                        /* Perubahan kondisi player yang dimundurin */
+                        
+                        /* Perubahan kondisi di player */
+
+                        /* Perbuahan kondisi di Map */
+
+                    /* Kalo SKILL == "Baling Baling Jambu" */
+                        /* Masukin username yang mau dimajuin */
+
+                        /* Perubahan kondisi player yang dimajuin */
+                        
+                        /* Perubahan kondisi di player */
+
+                        /* Perbuahan kondisi di Map */
+                    
+                    /* Kalo SKILL == "Cermin Pengganda" */
+                        /* Perubahan kondisi di player */
+
+                    /* Kalo SKILL == "Senter Pembesar Hoki" */
+                        /* ROLL dadu pake efek senter pembesar hoki */
+                        
+                        /* Perubahan kondisi di player */
+
+                        /* Perbuahan kondisi di Map */
+
+                    /* Kalo SKILL == "Senter Pengecil Hoki" */
+                        /* ROLL dadu pake efek senter pengecil hoki */
+                        
+                        /* Perubahan kondisi di player */
+
+                        /* Perbuahan kondisi di Map */
+
+                    /* Kalo SKILL == "Mesin Penukar Posisi" */
+                        /* Masukin username yang mau dituker sama current player */
+                        
+                        /* Perubahan kondisi di player yang dituker */
+
+                        /* Perubahan kondisi player current */
+
+                        /* Perbuahan kondisi di Map */
+
+                    /* Kalo SKILL == "Teknologi Gagal" */
+                        /* NOTHING HAPPEN */
+                    
+            
+            /* Kalo Commandnya MAP */
+
+            /* Kalo Commandnya BUFF */
+
+            /* Kalo Commandnya INSPECT */
+
+            /* Kalo Commandnya ROLL */
+
+            /* Kalo Commandnya SAVE */
+                /* Update semua player txt */
+
+                /* Update semua map txt */
+
+            /* Kalo Commandnya ENDTURN */
+                nextPlayer = TRUE;
+
+            /* Kalo Commandnya UNDO */
+                /* ini kita yang meninggal sebagai developer */
 
 
-            if (idxCurrentPlayer == banyakPemain) {
-                idxCurrentPlayer = 1;
-            }
-            else {
-                idxCurrentPlayer++;
+            /* ini buat gantian giliran player */
+            if (nextPlayer) {
+                if (idxCurrentPlayer == banyakPemain) {
+                    idxCurrentPlayer = 1;
+                }
+                else {
+                    idxCurrentPlayer++;
+                }
             }
 
         }
