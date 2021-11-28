dbc297ed99ee8915fd142aa762069948d45596c1
aldifadlian <16520443@std.stei.itb.ac.id>
2021-11-28 21:36:29 +0700

    file asistensi sama typo di nama map

--- src/ADT/map/map.c
+++ src/ADT/map/map.c
@@ -9,7 +9,7 @@ void inputConfig(Map *CurrentMap)
     TELEPORT_NEFF(*CurrentMap) = 0;
 
     while(!valid){
-        printf("\nKONFIGURASI MAP YANG TERSEDIA : \n\n1.RAT-RACE(rat-race.txt) -> Difficulty 9/10, Fun 3/10\n2.FLOOR-ISLAVA(lavafloor.txt) -> Difficulty 6/10, Fun 7/10\n\n"); 
+        printf("\nKONFIGURASI MAP YANG TERSEDIA : \n\n1.RAT-RACE(ratrace.txt) -> Difficulty 9/10, Fun 3/10\n2.FLOOR-ISLAVA(lavafloor.txt) -> Difficulty 6/10, Fun 7/10\n\n"); 
         printf("Masukkan file config txt : ");
         scanf("%s", namafile);
         if((strcmp(namafile, "ratrace.txt") == 0) || (strcmp(namafile, "lavafloor.txt") == 0)) {
