9faf35b47d101892af4499cdf1f37571142b9e5c
aldifadlian <16520443@std.stei.itb.ac.id>
2021-11-28 19:37:49 +0700

    debugging map

--- src/ADT/map/map.c
+++ src/ADT/map/map.c
@@ -17,7 +17,7 @@ void inputConfig(Map *CurrentMap)
             valid = TRUE;
         } else {
             printf("\nMasukkan salah!");
-            printf("\nKONFIGURASI MAP YANG TERSEDIA : \n\n1.RAT-RACE(rat-race.txt) -> Difficulty 9/10, Fun 3/10\n2.FLOOR-ISLAVA(lavafloor.txt) -> Difficulty 6/10, Fun 7/10\n\n"); 
+            printf("\nKONFIGURASI MAP YANG TERSEDIA : \n\n1.RAT-RACE(ratrace.txt) -> Difficulty 9/10, Fun 3/10\n2.FLOOR-ISLAVA(lavafloor.txt) -> Difficulty 6/10, Fun 7/10\n\n"); 
         }
     }
     readConfig(CurrentMap);
@@ -65,7 +65,7 @@ void printConfig(Map *CurrentMap)
 {
     printf("Panjang map : %d\n", MAP_LENGTH(*CurrentMap));
     printf("Layout map : ");
-    for(int i = 0; i < MAP_LENGTH(*CurrentMap); i++) {
+    for(int i = 1; i <= MAP_LENGTH(*CurrentMap); i++) {
         printf("%c", MAP_LAYOUT(*CurrentMap)[i]);
     }
     printf("\n");
